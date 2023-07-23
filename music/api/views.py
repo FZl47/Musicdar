e29303fa7adc07759bfaa54544f70bc2df50d7b2
Fazel Momeni <wwwfazel88@gmail.com>
2023-07-23 22:58:06 +0330

    some modify in api view and change status code response to 201

--- music/api/views.py
+++ music/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from core.api.exceptions import Conflict
 from music import models
-
 from . import serializers
 
 
@@ -19,14 +19,17 @@ class Music(APIView):
         s.is_valid(raise_exception=True)
         data = s.validated_data
         # get Artist object by name or create
-        artist_name = data['artist_name']
-        artist_obj = models.Artist.objects.get_by_name_or_create(artist_name)
+        artist_name = data.get('artist_name', None)
+        if artist_name:
+            artist_obj = models.Artist.objects.get_by_name_or_create(artist_name)
+        else:
+            artist_obj = models.Artist.objects.get_default()
         # get Category object by name or get default
-        category_name = data['category_name']
+        category_name = data.get('category_name', '')
         category_obj = models.Category.objects.get_by_name_or_default(category_name)
 
-        del data['artist_name']
-        del data['category_name']
+        data.pop('artist_name', None)
+        data.pop('category_name', None)
 
         music_obj = models.Music.objects.create_unique_or_none(**data,
                                                                artist=artist_obj,
@@ -34,7 +37,7 @@ class Music(APIView):
         if music_obj is None:
             # Music is duplicate
             raise Conflict('Music is duplicate')
-        return Response(serializers.MusicResponseSerializer(music_obj).data)
+        return Response(serializers.MusicResponseSerializer(music_obj).data,status=status.HTTP_201_CREATED)
 
 
 class Artist(APIView):
