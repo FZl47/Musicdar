ce24192d700cca5f511ca4d081cebd7b0581cba3
Fazel Momeni <wwwfazel88@gmail.com>
2023-07-23 22:58:54 +0330

    add objects to context view

--- public/views.py
+++ public/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from music.models import Music, Artist, News, Podcast
 
 
 def index(request):
-    return render(request,'index.html')
+    context = {
+        'latest_musics': Music.objects.get_latest(),
+        'trend_musics': Music.objects.get_trends(),
+        'latest_podcasts': Podcast.objects.get_latest(),
+        'artists': Artist.objects.get_random(12),
+        'latest_news': News.objects.get_latest()
+    }
+    return render(request, 'index.html', context)
