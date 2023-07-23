710ea4d27d59271f460ad068393c261dac3e3811
Fazel Momeni <wwwfazel88@gmail.com>
2023-07-23 23:01:59 +0330

    add serialisers api

---
+++ music/api/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework.serializers import ModelSerializer
+from rest_framework.exceptions import ValidationError
+from rest_framework import serializers
+from music import models
+
+
+class MusicSerializer(ModelSerializer):
+    category_name = serializers.CharField(max_length=200, required=False)
+    artist_name = serializers.CharField(max_length=20, required=False)
+
+    class Meta:
+        model = models.Music
+        exclude = ('id', 'views', 'artist', 'category')
+
+    def validate(self, data):
+        link_320 = data.get('link_320', None)
+        link_128 = data.get('link_128', None)
+        if not (link_320 or link_128):
+            raise ValidationError('At least one of the links must be set (link_320 or link_128)')
+        return data
+
+
+class MusicResponseSerializer(ModelSerializer):
+    url = serializers.URLField(source='get_absolute_url')
+
+    class Meta:
+        model = models.Music
+        fields = ('sku', 'name', 'url')
